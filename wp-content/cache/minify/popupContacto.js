46a0c8e296e037f869ede90a66de7eda8928bc1c
Spectral <bernamn24@hotmail.com>
2021-09-20 14:21:42 -0500

    Create popupContacto.js

---
+++ wp-content/cache/minify/popupContacto.js
@@ -0,0 +1,339 @@
+//GUATEMALA MAPA FOOTER
+
+var btnAbrirPopupEM = document.getElementById('mapa-guat'),
+  overlayEM = document.getElementById('overlay'),
+  popupEM = document.getElementById('popup'),
+  btnCerrarPopupEM = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopupEM.addEventListener('click', function() {
+  overlayEM.classList.add('active')
+  popupEM.classList.add('active')
+})
+
+btnCerrarPopupEM.addEventListener('click', function() {
+  overlayEM.classList.remove('active')
+  popupEM.classList.remove('active')
+})
+
+// ALTA VERAPAZ MAPA FOOTER
+
+var btnAbrirPopupAVEM = document.getElementById('mapa-alta'),
+  overlayAVEM = document.getElementById('overlayAV'),
+  popupAVEM = document.getElementById('popupAV'),
+  btnCerrarPopupAVEM = document.getElementById('btn-cerrar-popupAV');
+
+btnAbrirPopupAVEM.addEventListener('click', function() {
+  overlayAVEM.classList.add('active')
+  popupAVEM.classList.add('active')
+})
+
+btnCerrarPopupAVEM.addEventListener('click', function() {
+  overlayAVEM.classList.remove('active')
+  popupAVEM.classList.remove('active')
+})
+
+// BAJA VERAPAZ MAPA FOOTER
+
+var btnAbrirPopupBVEM = document.getElementById('mapa-baja'),
+  overlayBVEM = document.getElementById('overlayBV'),
+  popupBVEM = document.getElementById('popupBV'),
+  btnCerrarPopupBVEM = document.getElementById('btn-cerrar-popupBV');
+
+btnAbrirPopupBVEM.addEventListener('click', function() {
+  overlayBVEM.classList.add('active')
+  popupBVEM.classList.add('active')
+})
+
+btnCerrarPopupBVEM.addEventListener('click', function() {
+  overlayBVEM.classList.remove('active')
+  popupBVEM.classList.remove('active')
+})
+
+// CHIMALTENANGO MAPA FOOTER
+
+var btnAbrirPopupCHEM = document.getElementById('mapa-chima'),
+  overlayCHEM = document.getElementById('overlayCH'),
+  popupCHEM = document.getElementById('popupCH'),
+  btnCerrarPopupCHEM = document.getElementById('btn-cerrar-popupCH');
+
+btnAbrirPopupCHEM.addEventListener('click', function() {
+  overlayCHEM.classList.add('active')
+  popupCHEM.classList.add('active')
+})
+
+btnCerrarPopupCHEM.addEventListener('click', function() {
+  overlayCHEM.classList.remove('active')
+  popupCHEM.classList.remove('active')
+})
+
+//CHIQUIMULA MAPA FOOTER
+
+var btnAbrirPopupCHIEM = document.getElementById('mapa-chiqui'),
+  overlayCHIEM = document.getElementById('overlayCHI'),
+  popupCHIEM = document.getElementById('popupCHI'),
+  btnCerrarPopupCHIEM = document.getElementById('btn-cerrar-popupCHI');
+
+btnAbrirPopupCHIEM.addEventListener('click', function() {
+  overlayCHIEM.classList.add('active')
+  popupCHIEM.classList.add('active')
+})
+
+btnCerrarPopupCHIEM.addEventListener('click', function() {
+  overlayCHIEM.classList.remove('active')
+  popupCHIEM.classList.remove('active')
+})
+
+//ESCUINTLA MAPA FOOTER
+
+var btnAbrirPopupESEM = document.getElementById('mapa-esc'),
+  overlayESEM = document.getElementById('overlayES'),
+  popupESEM = document.getElementById('popupES'),
+  btnCerrarPopupESEM = document.getElementById('btn-cerrar-popupES');
+
+btnAbrirPopupESEM.addEventListener('click', function() {
+  overlayESEM.classList.add('active')
+  popupESEM.classList.add('active')
+})
+
+btnCerrarPopupESEM.addEventListener('click', function() {
+  overlayESEM.classList.remove('active')
+  popupESEM.classList.remove('active')
+})
+
+//HUEHUETENANGO MAPA FOOTER
+
+var btnAbrirPopupHUEEM = document.getElementById('mapa-hue'),
+  overlayHUEEM = document.getElementById('overlayHUE'),
+  popupHUEEM = document.getElementById('popupHUE'),
+  btnCerrarPopupHUEEM = document.getElementById('btn-cerrar-popupHUE');
+
+btnAbrirPopupHUEEM.addEventListener('click', function() {
+  overlayHUEEM.classList.add('active')
+  popupHUEEM.classList.add('active')
+})
+
+btnCerrarPopupHUEEM.addEventListener('click', function() {
+  overlayHUEEM.classList.remove('active')
+  popupHUEEM.classList.remove('active')
+})
+
+//IZABAL MAPA FOOTER
+
+var btnAbrirPopupIZAEM = document.getElementById('mapa-iza'),
+  overlayIZAEM = document.getElementById('overlayIZA'),
+  popupIZAEM = document.getElementById('popupIZA'),
+  btnCerrarPopupIZAEM = document.getElementById('btn-cerrar-popupIZA');
+
+btnAbrirPopupIZAEM.addEventListener('click', function() {
+  overlayIZAEM.classList.add('active')
+  popupIZAEM.classList.add('active')
+})
+
+btnCerrarPopupIZAEM.addEventListener('click', function() {
+  overlayIZAEM.classList.remove('active')
+  popupIZAEM.classList.remove('active')
+})
+
+//JALAPA MAPA FOOTER
+
+var btnAbrirPopupJALEM = document.getElementById('mapa-jal'),
+  overlayJALEM = document.getElementById('overlayJAL'),
+  popupJALEM = document.getElementById('popupJAL'),
+  btnCerrarPopupJALEM = document.getElementById('btn-cerrar-popupJAL');
+
+btnAbrirPopupJALEM.addEventListener('click', function() {
+  overlayJALEM.classList.add('active')
+  popupJALEM.classList.add('active')
+})
+
+btnCerrarPopupJALEM.addEventListener('click', function() {
+  overlayJALEM.classList.remove('active')
+  popupJALEM.classList.remove('active')
+})
+
+//JUTIAPA MAPA FOOTER
+
+var btnAbrirPopupJUTEM = document.getElementById('mapa-jut'),
+  overlayJUTEM = document.getElementById('overlayJUT'),
+  popupJUTEM = document.getElementById('popupJUT'),
+  btnCerrarPopupJUTEM = document.getElementById('btn-cerrar-popupJUT');
+
+btnAbrirPopupJUTEM.addEventListener('click', function() {
+  overlayJUTEM.classList.add('active')
+  popupJUTEM.classList.add('active')
+})
+
+btnCerrarPopupJUTEM.addEventListener('click', function() {
+  overlayJUTEM.classList.remove('active')
+  popupJUTEM.classList.remove('active')
+})
+
+//PETÉN MAPA FOOTER
+
+var btnAbrirPopupPETEM = document.getElementById('mapa-pet'),
+  overlayPETEM = document.getElementById('overlayPET'),
+  popupPETEM = document.getElementById('popupPET'),
+  btnCerrarPopupPETEM = document.getElementById('btn-cerrar-popupPET');
+
+btnAbrirPopupPETEM.addEventListener('click', function() {
+  overlayPETEM.classList.add('active')
+  popupPETEM.classList.add('active')
+})
+
+btnCerrarPopupPETEM.addEventListener('click', function() {
+  overlayPETEM.classList.remove('active')
+  popupPETEM.classList.remove('active')
+})
+
+//EL PROGRESO MAPA FOOTER
+
+var btnAbrirPopupPROEM = document.getElementById('mapa-pro'),
+  overlayPROEM = document.getElementById('overlayPRO'),
+  popupPROEM = document.getElementById('popupPRO'),
+  btnCerrarPopupPROEM = document.getElementById('btn-cerrar-popupPRO');
+
+btnAbrirPopupPROEM.addEventListener('click', function() {
+  overlayPROEM.classList.add('active')
+  popupPROEM.classList.add('active')
+})
+
+btnCerrarPopupPROEM.addEventListener('click', function() {
+  overlayPROEM.classList.remove('active')
+  popupPROEM.classList.remove('active')
+})
+
+//QUETZALTENANGO MAPA FOOTER
+
+var btnAbrirPopupQUEEM = document.getElementById('mapa-quetz'),
+  overlayQUEEM = document.getElementById('overlayQUE'),
+  popupQUEEM = document.getElementById('popupQUE'),
+  btnCerrarPopupQUEEM = document.getElementById('btn-cerrar-popupQUE');
+
+btnAbrirPopupQUEEM.addEventListener('click', function() {
+  overlayQUEEM.classList.add('active')
+  popupQUEEM.classList.add('active')
+})
+
+btnCerrarPopupQUEEM.addEventListener('click', function() {
+  overlayQUEEM.classList.remove('active')
+  popupQUEEM.classList.remove('active')
+})
+
+//RETALHULEU MAPA FOOTER
+
+var btnAbrirPopupRETEM = document.getElementById('mapa-reta'),
+  overlayRETEM = document.getElementById('overlayRET'),
+  popupRETEM = document.getElementById('popupRET'),
+  btnCerrarPopupRETEM = document.getElementById('btn-cerrar-popupRET');
+
+btnAbrirPopupRETEM.addEventListener('click', function() {
+  overlayRETEM.classList.add('active')
+  popupRETEM.classList.add('active')
+})
+
+btnCerrarPopupRETEM.addEventListener('click', function() {
+  overlayRETEM.classList.remove('active')
+  popupRETEM.classList.remove('active')
+})
+
+//QUICHÉ MAPA FOOTER
+
+var btnAbrirPopupQUIEM = document.getElementById('mapa-qui'),
+  overlayQUIEM = document.getElementById('overlayQUI'),
+  popupQUIEM = document.getElementById('popupQUI'),
+  btnCerrarPopupQUIEM = document.getElementById('btn-cerrar-popupQUI');
+
+btnAbrirPopupQUIEM.addEventListener('click', function() {
+  overlayQUIEM.classList.add('active')
+  popupQUIEM.classList.add('active')
+})
+
+btnCerrarPopupQUIEM.addEventListener('click', function() {
+  overlayQUIEM.classList.remove('active')
+  popupQUIEM.classList.remove('active')
+})
+
+//SACATEPÉQUEZ MAPA FOOTER
+
+var btnAbrirPopupSACEM = document.getElementById('mapa-sac'),
+  overlaySACEM = document.getElementById('overlaySAC'),
+  popupSACEM = document.getElementById('popupSAC'),
+  btnCerrarPopupSACEM = document.getElementById('btn-cerrar-popupSAC');
+
+btnAbrirPopupSACEM.addEventListener('click', function() {
+  overlaySACEM.classList.add('active')
+  popupSACEM.classList.add('active')
+})
+
+btnCerrarPopupSACEM.addEventListener('click', function() {
+  overlaySACEM.classList.remove('active')
+  popupSACEM.classList.remove('active')
+})
+
+//SAN MARCOS MAPA FOOTER
+
+var btnAbrirPopupSANEM = document.getElementById('mapa-san'),
+  overlaySANEM = document.getElementById('overlaySAN'),
+  popupSANEM = document.getElementById('popupSAN'),
+  btnCerrarPopupSANEM = document.getElementById('btn-cerrar-popupSAN');
+
+btnAbrirPopupSANEM.addEventListener('click', function() {
+  overlaySANEM.classList.add('active')
+  popupSANEM.classList.add('active')
+})
+
+btnCerrarPopupSANEM.addEventListener('click', function() {
+  overlaySANEM.classList.remove('active')
+  popupSANEM.classList.remove('active')
+})
+
+//SUCHITEPÉQUEZ MAPA FOOTER
+
+var btnAbrirPopupSUCHEM = document.getElementById('mapa-such'),
+  overlaySUCHEM = document.getElementById('overlaySUCH'),
+  popupSUCHEM = document.getElementById('popupSUCH'),
+  btnCerrarPopupSUCHEM = document.getElementById('btn-cerrar-popupSUCH');
+
+btnAbrirPopupSUCHEM.addEventListener('click', function() {
+  overlaySUCHEM.classList.add('active')
+  popupSUCHEM.classList.add('active')
+})
+
+btnCerrarPopupSUCHEM.addEventListener('click', function() {
+  overlaySUCHEM.classList.remove('active')
+  popupSUCHEM.classList.remove('active')
+})
+
+//TOTONICAPÁN MAPA FOOTER
+
+var btnAbrirPopupTOTEM = document.getElementById('mapa-tot'),
+  overlayTOTEM = document.getElementById('overlayTOT'),
+  popupTOTEM = document.getElementById('popupTOT'),
+  btnCerrarPopupTOTEM = document.getElementById('btn-cerrar-popupTOT');
+
+btnAbrirPopupTOTEM.addEventListener('click', function() {
+  overlayTOTEM.classList.add('active')
+  popupTOTEM.classList.add('active')
+})
+
+btnCerrarPopupTOTEM.addEventListener('click', function() {
+  overlayTOTEM.classList.remove('active')
+  popupTOTEM.classList.remove('active')
+})
+
+//ZACAPA MAPA FOOTER
+
+var btnAbrirPopupZACEM = document.getElementById('mapa-zac'),
+  overlayZACEM = document.getElementById('overlayZAC'),
+  popupZACEM = document.getElementById('popupZAC'),
+  btnCerrarPopupZACEM = document.getElementById('btn-cerrar-popupZAC');
+
+btnAbrirPopupZACEM.addEventListener('click', function() {
+  overlayZACEM.classList.add('active')
+  popupZACEM.classList.add('active')
+})
+
+btnCerrarPopupZACEM.addEventListener('click', function() {
+  overlayZACEM.classList.remove('active')
+  popupZACEM.classList.remove('active')
+})
